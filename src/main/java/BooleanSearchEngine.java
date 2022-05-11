9166c0e08cf769ac6cc4c6bb88eeaa3b8ba6f981
ruslanv-r <rvregi@gmail.com>
2022-05-11 12:04:17 +0900

    1

--- src/main/java/BooleanSearchEngine.java
+++ src/main/java/BooleanSearchEngine.java
@@ -1,13 +1,42 @@
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfReader;
+import com.itextpdf.kernel.pdf.canvas.parser.PdfTextExtractor;
+
 import java.io.File;
 import java.io.IOException;
 import java.util.*;
 
 public class BooleanSearchEngine implements SearchEngine {
-    //???
+    File pdfsDir;
+    Map<String, List<PageEntry>> indexPDF = new HashMap<>();
 
     public BooleanSearchEngine(File pdfsDir) throws IOException {
+        this.pdfsDir = pdfsDir;
+
         // прочтите тут все pdf и сохраните нужные данные,
         // тк во время поиска сервер не должен уже читать файлы
+
+        var doc = new PdfDocument(new PdfReader(pdf));
+//        doc.getLastPage();
+        int amountPage = doc.getPageNumber(doc.getLastPage());
+
+        for (int i = 0; i < amountPage - 1; i++) {
+            var page = doc.getPage(i);
+            var text = PdfTextExtractor.getTextFromPage(page);
+            var words = text.split("\\P{IsAlphabetic}+");
+
+            Map<String, Integer> freqs = new HashMap<>();
+            for (String word : words) {
+                if (word.isEmpty()) {
+                    continue;
+                }
+                freqs.put(word.toLowerCase(),freqs.getOrDefault(word, 0)+1);
+            }
+
+
+        }
+
+
     }
 
     @Override
--- src/main/java/PageEntry.java
+++ src/main/java/PageEntry.java
@@ -3,5 +3,47 @@ public class PageEntry implements Comparable<PageEntry> {
     private final int page;
     private final int count;
 
-    // ???
+    public PageEntry(String pdfName, int page, int count) {
+        this.pdfName = pdfName;
+        this.page = page;
+        this.count = count;
+    }
+
+    @Override
+    public int compareTo(PageEntry o) {
+
+        int result = 2;
+        if (this.count < o.count) {
+            result = 1;
+        } else if (this.count > o.count) {
+            result = -1;
+        } else {
+            result = 0;
+        }
+
+        return result;
+
+
+    }
+
+    public String getPdfName() {
+        return pdfName;
+    }
+
+    public int getPage() {
+        return page;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    @Override
+    public String toString() {
+        return "PageEntry{" +
+                "pdfName='" + pdfName + '\'' +
+                ", page=" + page +
+                ", count=" + count +
+                '}';
+    }
 }
