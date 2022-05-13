61cb63cf8fb8e8c76bdc69403cc4ddfa4251a8dd
ruslanv-r <rvregi@gmail.com>
2022-05-13 10:04:12 +0900

    прооверка индексации

--- src/main/java/BooleanSearchEngine.java
+++ src/main/java/BooleanSearchEngine.java
@@ -7,36 +7,60 @@ import java.io.IOException;
 import java.util.*;
 
 public class BooleanSearchEngine implements SearchEngine {
+
     File pdfsDir;
     Map<String, List<PageEntry>> indexPDF = new HashMap<>();
 
     public BooleanSearchEngine(File pdfsDir) throws IOException {
         this.pdfsDir = pdfsDir;
 
+        List<PageEntry> pageEntryList = new ArrayList<>();  //нужна ли эта коллекция?
+
         // прочтите тут все pdf и сохраните нужные данные,
         // тк во время поиска сервер не должен уже читать файлы
 
-        var doc = new PdfDocument(new PdfReader(pdf));
+        StringJoiner sj = new StringJoiner(", "); //
+
+        for (File pdf : pdfsDir.listFiles()) {              //перебор по пдф файлам
+
+
+            var doc = new PdfDocument(new PdfReader(pdf));
 //        doc.getLastPage();
-        int amountPage = doc.getPageNumber(doc.getLastPage());
+            int amountPage = doc.getPageNumber(doc.getLastPage());
 
-        for (int i = 0; i < amountPage - 1; i++) {
-            var page = doc.getPage(i);
-            var text = PdfTextExtractor.getTextFromPage(page);
-            var words = text.split("\\P{IsAlphabetic}+");
 
-            Map<String, Integer> freqs = new HashMap<>();
-            for (String word : words) {
-                if (word.isEmpty()) {
-                    continue;
+            for (int i = 1; i < amountPage; i++) {
+                var page = doc.getPage(i);
+                var text = PdfTextExtractor.getTextFromPage(page);
+                var words = text.split("\\P{IsAlphabetic}+");
+
+                Map<String, Integer> freqs = new HashMap<>();
+                for (String word : words) {
+                    if (word.isEmpty()) {
+                        continue;
+                    }
+                    freqs.put(word.toLowerCase(), freqs.getOrDefault(word, 0) + 1);
                 }
-                freqs.put(word.toLowerCase(),freqs.getOrDefault(word, 0)+1);
+
+                for (Map.Entry<String, Integer> item : freqs.entrySet()) {
+                    pageEntryList.add(new PageEntry(pdf.getName(), i, item.getValue()));
+                    indexPDF.put(item.getKey(), pageEntryList);
+                    sj.add("Слово - " + item.getKey() + " = " + pdf.getName() + " стр: " + i + " колич: " + item.getValue());   //
+                }
+
             }
-
-
         }
 
+        String ddd = sj.toString();
+        System.out.println(ddd);
 
+//        File resut = new  File ("pdfsDir", "result");
+//
+//        for (Map.Entry<String, List<PageEntry>> entry : indexPDF.entrySet()) {
+//            System.out.println(entry);
+//
+//        }
+//        System.out.println(indexPDF);
     }
 
     @Override
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,9 @@
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.Arrays;
 
 public class Main {
@@ -6,8 +11,40 @@ public class Main {
         BooleanSearchEngine engine = new BooleanSearchEngine(new File("pdfs"));
         System.out.println(engine.search("бизнес"));
 
+
+
+
+
         // здесь создайте сервер, который отвечал бы на нужные запросы
         // слушать он должен порт 8989
         // отвечать на запросы /{word} -> возвращённое значение метода search(word) в JSON-формате
+        int port = 8989;
+
+
+//        while (true) {
+//
+//            System.out.println("Starting server at " + port + "...");
+//            ServerSocket serverSocket = new ServerSocket(port);
+//            Socket connection = serverSocket.accept();
+//            PrintWriter out = new PrintWriter(connection.getOutputStream(), true); //исх
+//            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream())); //вх
+//            System.out.println("Port - " + connection.getPort());
+//            String inValue= in.readLine();
+//            System.out.println(inValue);
+//
+//
+//
+//
+//
+//        }
+
+
+
+
+
+
+
+
+
     }
 }
